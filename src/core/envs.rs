3214b8cd7b99f97e3ce283b7b0af71830f35a240
Théo ELOY <theoeloy12@gmail.com>
2025-05-31 15:50:11 +0200

    Add StatefulEnv trait for converting a montecarlo env to a series of matrix for dynamic programming

--- src/core/envs.rs
+++ src/core/envs.rs
@@ -1,10 +1,14 @@
-mod environments {
-    pub trait Environment {
-        fn reset(&mut self) -> usize; // renvoie l’état initial
-        fn step(&mut self, action: usize) -> (usize, f64, bool); // (next_state, reward, done)
-        fn num_states(&self) -> usize;
-        fn num_actions(&self) -> usize;
-        
-        //fn render(&self);
-    }
+pub trait Environment {
+    fn reset(&mut self); // renvoie l’état initial
+    fn step(&mut self, action: usize); // (next_state, reward, done)
+    fn score(&self) -> f64;
+    fn num_states(&self) -> usize;
+    fn num_actions(&self) -> usize;
+    fn is_game_over(&self) -> bool;
+    //fn render(&self);
+}
+
+pub trait StatefulEnvironment: Environment {
+    fn set_state(&self, state: usize);
+    fn get_state(&self) -> usize;
 }
