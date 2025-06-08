db2af1754cc722d036e515175981d5e10652ad82
Théo ELOY <theoeloy12@gmail.com>
2025-06-08 16:30:01 +0200

    Add Dynamic Programming for Line world Env

--- src/core/envs.rs
+++ src/core/envs.rs
@@ -1,4 +1,4 @@
-pub trait Environment {
+pub trait MonteCarloEnvironment {
     fn reset(&mut self); // renvoie l’état initial
     fn step(&mut self, action: usize); // (next_state, reward, done)
     fn score(&self) -> f64;
@@ -8,7 +8,94 @@ pub trait Environment {
     //fn render(&self);
 }
 
-pub trait StatefulEnvironment: Environment {
-    fn set_state(&self, state: usize);
-    fn get_state(&self) -> usize;
+/// Permet de représenter un environnement pour l'utiliser avec les fontions de dynamic programming
+#[derive(Debug)]
+pub struct DPEnvironment {
+    pub states: Vec<usize>,
+    pub actions: Vec<usize>,
+
+    // Vecteur listant toutes les rewards possibles
+    pub rewards: Vec<f64>,
+    pub terminal_states: Vec<usize>,
+
+    // Vecteur représentant les probabilités de transitions entre les
+    // états de forme P[state, action, state_prime, reward_index]
+    pub transitions: Vec<f64>,
+
+    pub num_states: usize,
+    pub num_actions: usize,
+    pub num_rewards: usize,
+}
+
+impl DPEnvironment {
+    /// Constructeur statique pour initialiser l'environnement avec des transitions nulles
+    pub fn new(
+        num_states: usize,
+        num_actions: usize,
+        num_rewards: usize,
+        rewards: Vec<f64>,
+        terminal_states: Vec<usize>,
+    ) -> Self {
+        let transitions_size = num_states * num_actions * num_states * num_rewards;
+        DPEnvironment {
+            states: (0..num_states).collect(),
+            actions: (0..num_actions).collect(),
+            rewards,
+            terminal_states,
+            transitions: vec![0.0; transitions_size],
+            num_states,
+            num_actions,
+            num_rewards,
+        }
+    }
+
+    /// Calcule l'index aplati à partir des indices 4D
+    fn get_index(
+        &self,
+        state: usize,
+        action: usize,
+        state_prime: usize,
+        reward_index: usize,
+    ) -> usize {
+        assert!(state < self.num_states, "state index out of bounds");
+        assert!(action < self.num_actions, "action index out of bounds");
+        assert!(
+            state_prime < self.num_states,
+            "state_prime index out of bounds"
+        );
+        assert!(
+            reward_index < self.num_rewards,
+            "reward_index index out of bounds"
+        );
+
+        state * (self.num_actions * self.num_states * self.num_rewards)
+            + action * (self.num_states * self.num_rewards)
+            + state_prime * self.num_rewards
+            + reward_index
+    }
+
+    /// Retourne la probabilité de transition pour un tuple (state, action, state_prime, reward_index)
+    pub fn get_transition_prob(
+        &self,
+        state: usize,
+        action: usize,
+        state_prime: usize,
+        reward_index: usize,
+    ) -> f64 {
+        let index = self.get_index(state, action, state_prime, reward_index);
+        self.transitions[index]
+    }
+
+    /// Permet de modifier la probabilité de transition pour un tuple (state, action, state_prime, reward_index)
+    pub fn set_transition_prob(
+        &mut self,
+        state: usize,
+        action: usize,
+        state_prime: usize,
+        reward_index: usize,
+        value: f64,
+    ) {
+        let index = self.get_index(state, action, state_prime, reward_index);
+        self.transitions[index] = value;
+    }
 }
--- src/environments/line_world.rs
+++ src/environments/line_world.rs
@@ -1,10 +1,11 @@
-use crate::core::envs::Environment;
+use crate::core::envs;
+use crate::core::envs::DPEnvironment;
 
 struct LineWorld {
     agent_pos: usize,
 }
 
-impl Environment for LineWorld {
+impl envs::MonteCarloEnvironment for LineWorld {
     fn reset(&mut self) {
         self.agent_pos = 2;
     }
@@ -40,4 +41,19 @@ impl Environment for LineWorld {
     fn is_game_over(&self) -> bool {
         self.agent_pos == 0 || self.agent_pos == 4
     }
+}
+
+
+pub fn line_world_dp() -> DPEnvironment {
+    let mut env = DPEnvironment::new(5, 2, 3, vec![-1.0, 0.0, 1.0], vec![1, 4]);
+    
+    env.set_transition_prob(3,0,2,1,1.0);
+    env.set_transition_prob(2,0,1,1,1.0);
+    env.set_transition_prob(1,0,0,0,1.0);
+
+    env.set_transition_prob(3,1,4,2,1.0);
+    env.set_transition_prob(2,1,3,1,1.0);
+    env.set_transition_prob(1,1,2,1,1.0);
+    
+    env
 }
