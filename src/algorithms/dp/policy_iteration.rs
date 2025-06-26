a5ccc35d057dd96407bbb5a6ad51df251e41a601
Théo ELOY <theoeloy12@gmail.com>
2025-06-26 23:36:59 +0200

    :sparkles: Implement `policy_iteration` with evaluation and improvement functions

--- src/algorithms/dp/policy_iteration.rs
+++ src/algorithms/dp/policy_iteration.rs
@@ -1,11 +1,11 @@
 use crate::core::envs::DPEnvironment;
-use crate::core::policies::{Policy, ProbabilisticPolicy};
+use crate::core::policies::{DeterministicPolicy, Policy, ProbabilisticPolicy};
 
 pub fn policy_evaluation(
-    env:    &DPEnvironment,
-    policy: ProbabilisticPolicy,
-    theta:  f64,
-    gamma:  f64,
+    env: &DPEnvironment,
+    policy: &DeterministicPolicy,
+    theta: f64,
+    gamma: f64,
 ) -> Vec<f64> {
     let mut values = vec![0.0; env.num_states];
 
@@ -17,25 +17,17 @@ pub fn policy_evaluation(
         let mut delta: f64 = 0.0;
         for s in &env.states {
             let v_old: f64 = values[*s];
-            // 1) initialiser total pour CHAQUE état
             let mut total = 0.0;
 
-            // 2) pour chaque action, calculer Q(s,a)
-            for a in &env.actions {
-                let mut action_value = 0.0;
-                for s_prime in &env.states {
-                    for r_index in 0..env.num_rewards {
-                        let r = env.rewards[r_index];
-                        let p = env.get_transition_prob(*s, *a, *s_prime, r_index);
-                        // 3) utiliser r + γ * V(s')
-                        action_value += p * (r + gamma * values[*s_prime]);
-                    }
+            for s_prime in &env.states {
+                for r_index in 0..env.num_rewards {
+                    let r = env.rewards[r_index];
+                    let a = policy.get_action(s);
+                    let p = env.get_transition_prob(*s, a, *s_prime, r_index);
+                    total += p * (r + gamma * values[*s_prime]);
                 }
-                // 4) pondérer par π(a|s)
-                total += policy.get_proba(*s, *a) * action_value;
             }
 
-            // 5) mettre à jour V(s) **UNE FOIS** par état
             values[*s] = total;
             delta = delta.max((v_old - total).abs());
         }
@@ -48,26 +40,101 @@ pub fn policy_evaluation(
     values
 }
 
+pub fn policy_improvement(
+    env: &DPEnvironment,
+    old_policy: &DeterministicPolicy,
+    v: &Vec<f64>,
+    gamma: f64,
+) -> (DeterministicPolicy, bool) {
+    let mut new_policy = DeterministicPolicy::new_det_pol(env);
+    let mut policy_is_stable = true;
+
+    for s in &env.states {
+        let old_actions = old_policy.get_action(s);
+        let mut best_action: Option<usize> = None;
+        let mut best_value: f64 = f64::NEG_INFINITY;
+        for a in 0..env.num_actions {
+            let mut score = 0.0;
+            for s_prime in &env.states {
+                for r_index in 0..env.num_rewards {
+                    let r = env.rewards[r_index];
+                    let p = env.get_transition_prob(*s, a, *s_prime, r_index);
+                    score += p * (r + gamma * v[*s_prime]);
+                    if best_action.is_none() || score > best_value {
+                        best_action = Some(a);
+                        best_value = score;
+                    }
+                }
+                if best_action.unwrap() != old_actions {
+                    policy_is_stable = false;
+                }
+                new_policy.set_action(s, best_action.unwrap())
+            }
+        }
+    }
+    (new_policy, policy_is_stable)
+}
+
+pub fn policy_iteration(
+    env: &DPEnvironment,
+    theta: f64,
+    gamma: f64,
+) -> (DeterministicPolicy, Vec<f64>) {
+    let mut policy = DeterministicPolicy::new_det_pol(env);
+
+    let mut cpt = 100;
+    loop {
+        let v = policy_evaluation(env, &policy, theta, gamma);
+
+        let (new_policy, policy_is_stable) = policy_improvement(env, &policy, &v, gamma);
+
+        if !policy_is_stable {
+            return (new_policy, v);
+        }
+        policy = new_policy;
+
+        cpt -= 1;
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use crate::environments::line_world::line_world_dp;
+
     #[test]
     fn test_policy_evaluation() {
-        use super::*;
-        use crate::core::policies::ProbabilisticPolicy;
-        use crate::environments::line_world::line_world_dp;
-
         let env = line_world_dp();
-        let policy: ProbabilisticPolicy = ProbabilisticPolicy::new(&env);
+        let mut policy: DeterministicPolicy = DeterministicPolicy::new_det_pol(&env);
+        for s in &env.states {
+            policy.set_action(s, 1);
+        }
 
-        let result = policy_evaluation(&env, policy, 0.0001, 0.99);
+        let result = policy_evaluation(&env, &policy, 0.0001, 0.99);
 
-        let expected = vec![0.0, -0.5, 0.0, 0.5, 0.0];
+        let expected = vec![0.0, 1.0, 1.0, 1.0, 0.0];
         let rounded: Vec<f64> = result.iter().map(|v| (v * 100.0).round() / 100.0).collect();
         assert_eq!(
             rounded, expected,
             "Comparing {:?} and {:?}",
             result, expected
         );
+    }
+
+    #[test]
+    fn test_policy_iteration() {
+        let env = line_world_dp();
+
+        let (policy, values) = policy_iteration(&env, 0.0001, 0.99);
+        let expected = vec![0, 1, 1, 1, 0];
+
+        for s in 0..3 {
+            assert_eq!(
+                expected[s],
+                policy.get_action(&s),
+                "État {} doit aller à droite",
+                s
+            )
+        }
     }
 }
